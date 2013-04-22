9bc94be7ba67b08322c576ae1ea9a090fea9a33c
Kasper Videbæk <kaspervidebaek@gmail.com>
2013-04-22 17:51:09 +0200

    Added Roslyn Playground project

---
+++ Code/RoslynPlayGround/Program.cs
@@ -0,0 +1,24 @@
+﻿using Roslyn.Compilers.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynPlayGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReplaceTwoNodes();
+        }
+
+
+        static void ReplaceTwoNodes()
+        {
+            var tree = SyntaxTree.ParseText("class test { void test1() {} void test2{} }");
+            var members = tree.GetRoot().ChildNodes().First();
+        }
+    }
+}
